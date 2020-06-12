515298b8236b3ca796e5c4e93870d613da9a1706
Andy <asalmon9989@gmail.com>
2020-06-11 18:27:18 -0700

    reportevent and vieweventdetails

--- reportevent/index.js
+++ reportevent/index.js
@@ -1,16 +1,62 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const sql = require("mssql");
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+const sqlConfig = {
+    user: process.env.dbuser,
+    password: process.env.dbpassword,
+    server: process.env.dbserver,
+    database: process.env.database,
+};
+async function insertEvent(args) {
+    const sqlQuery = `insert into Events (eventId, eventType, userToken, eventDesc, lat, lon, reportedDt) 
+    values (newId(), @eventType, @userToken, @eventDesc, @lat, @lon, @reportedDt)
+    `;
+
+    const pool = await sql.connect(sqlConfig)
+    const ps = new sql.PreparedStatement(pool);
+    ps.input("eventType", sql.NVarChar);
+    ps.input("userToken", sql.NVarChar);
+    ps.input("eventDesc", sql.NVarChar);
+    ps.input("lat", sql.Decimal);
+    ps.input("lon", sql.Decimal);
+    ps.input("reportedDt", sql.BigInt);
+    await ps.prepare(sqlQuery)
+    const results = await ps.execute(args);
+    ps.unprepare();
+    // TODO: Return the id of the event
+    return results.rowsAffected;
+
+}
+// TODO: Error check sql
+module.exports = async function (context, req) {
+    const {
+        eventType,
+        userToken,
+        eventDesc,
+        coordinates,
+    } = req.body;
+    // TODO: Error check values
+    const results = await insertEvent({
+        eventType,
+        userToken,
+        eventDesc,
+        lat: coordinates.lat,
+        lon: coordinates.lon,
+        reportedDt: Date.now(),
+    });
+
+    context.res = {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+    };
+    if (results) {
+        context.res.body = {
+            message: "Event added"
+        }
+    } else {
+        context.res.body = {
+            message: "Unable to add event"
+        }
     }
 };
