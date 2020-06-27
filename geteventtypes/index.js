f22f7f34ef69caa2b4b17bd984497e85b54cc334
Andy <asalmon9989@gmail.com>
2020-06-27 15:06:42 -0700

    geteventtypes returns db content

--- geteventtypes/index.js
+++ geteventtypes/index.js
@@ -8,70 +8,17 @@ const sqlConfig = {
 };
 
 async function getSqlData() {
-  let sqlQuery = `select eventType, description from EventTypes where showInApp = 1`;
+  let sqlQuery = `select eventCategory, eventType, description from EventTypes where showInApp = 1 order by eventCategory, eventType`;
 
   let pool = await sql.connect(sqlConfig)
-  // let result1 = await pool.request()
-  //     .input('input_parameter', sql.Int, value)
-  //     .query('select * from mytable where id = @input_parameter')
+
   let result = await pool.request()
     .query(sqlQuery);
 
   // console.dir(result1)
 
+  return result.recordset;
 
-  //return result.recordset;
-  return [{
-      eventCategory: "POLICE",
-      eventType: "Police Type 1",
-      description: "Reported police event 1"
-    },
-    {
-      eventCategory: "POLICE",
-      eventType: "Police Type 2",
-      description: "Reported police event 2"
-    },
-    {
-      eventCategory: "AID",
-      eventType: "Aid Type 1",
-      description: "Reported aid event 1"
-    },
-    {
-      eventCategory: "AID",
-      eventType: "Aid Type 2",
-      description: "Reported aid event 2"
-    },
-    {
-      eventCategory: "INFO",
-      eventType: "Info Type 1",
-      description: "Reported info event 1"
-    },
-    {
-      eventCategory: "INFO",
-      eventType: "Info Type 2",
-      description: "Reported info event 2"
-    },
-    {
-      eventCategory: "SAFETY",
-      eventType: "Safety Type 1",
-      description: "Reported safety event 1"
-    },
-    {
-      eventCategory: "SAFETY",
-      eventType: "Safety Type 2",
-      description: "Reported safety event 2"
-    },
-    {
-      eventCategory: "SOS",
-      eventType: "SOS Type 1",
-      description: "Reported sos event 1"
-    },
-    {
-      eventCategory: "SOS",
-      eventType: "SOS Type 2",
-      description: "Reported sos event 2"
-    },
-  ]
 }
 
 module.exports = async function (context, req) {
