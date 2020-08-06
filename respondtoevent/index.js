4bc7b6ee93dc9426ad920d89cf7299af44af3312
Andy <asalmon9989@gmail.com>
2020-08-05 18:18:44 -0700

    Can only respond to an event once

--- respondtoevent/index.js
+++ respondtoevent/index.js
@@ -8,22 +8,35 @@ const sqlConfig = {
 };
 
 async function writeConfirm(eventId, userToken, isActive) {
-  let finalIsActive = 0
-  if(typeof isActive === "boolean") {
-    if(isActive) finalIsActive = 1;
+  let finalIsActive = 0;
+  let returnObj = {};
+  if (typeof isActive === "boolean") {
+    if (isActive) finalIsActive = 1;
   }
 
   try {
-    let currentTimestamp = Date.now();
-
-    let sqlQuery = `INSERT INTO EventResponses(eventId, userToken, reportedActive, responseDt)
-          VALUES(N'${eventId}', N'${userToken}', ${finalIsActive}, ${currentTimestamp})`;
-
+    let sqlQuery = `SELECT responseDt, reportedActive from EventResponses WHERE eventId=N'${eventId}' AND userToken=N'${userToken}'`;
     let pool = await sql.connect(sqlConfig);
     let result = await pool.request().query(sqlQuery);
+    let currentTimestamp = Date.now();
+    if (result.recordset.length > 0) {
+      // Maybe here we would check when the last time they responded was
+      sqlQuery = `UPDATE EventResponses SET reportedActive=${finalIsActive}, responseDt=${currentTimestamp}
+        WHERE eventId=N'${eventId}' AND userToken=N'${userToken}'`;
+      reselt = await pool.request().query(sqlQuery);
+      returnObj.action = "UPDATED";
+    } else {
+      sqlQuery = `INSERT INTO EventResponses(eventId, userToken, reportedActive, responseDt)
+            VALUES(N'${eventId}', N'${userToken}', ${finalIsActive}, ${currentTimestamp})`;
 
-    return result.rowsAffected;
-  } catch (ex) {}
+      result = await pool.request().query(sqlQuery);
+      returnObj.action = "INSERTED";
+    }
+
+  } catch (ex) {
+    returnObj.action = "ERROR";
+  }
+  return returnObj;
 }
 
 module.exports = async function (context, req) {
@@ -31,20 +44,32 @@ module.exports = async function (context, req) {
     // handle error here
   });
 
-  if (req.method == "POST") {
+  if (req.method === "POST") {
 
-    let recordsCreated = writeConfirm(req.body.eventId, req.body.userToken, req.body.eventActive);
+    let returnObj = await writeConfirm(req.body.eventId, req.body.userToken, req.body.eventActive);
 
-    if(recordsCreated) {
-        context.res = {
-          // status: 200, /* Defaults to 200 */
-          body: JSON.stringify({ message: "Update complete" })
-        };
-    } else {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: JSON.stringify({ message: "No Records Effected" })
-          };
+    if (returnObj.action === "INSERTED") {
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify({
+          message: "Response added"
+        })
+      };
+    } else if (returnObj.action === "UPDATED") {
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify({
+          message: "Response updated"
+        })
+      };
+    } else if (returnObj.action === "ERROR") {
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        status: 500,
+        body: JSON.stringify({
+          message: "Internal Error"
+        })
+      };
     }
   } else {
     context.res = {
